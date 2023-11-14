44487b9b8a9a86a50a12b609cdb8fdf0f6adab0c
theri <therigasmazurets@gmail.com>
2023-11-14 17:58:09 +0200

    up

---
+++ app/src/main/java/com/mazurets/currencyconverter/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mazurets.currencyconverter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mazurets.currencyconverter.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
